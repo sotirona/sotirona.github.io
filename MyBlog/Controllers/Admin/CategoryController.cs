c06894d88f268e1dd3bf5c60d3ce43a38a0377ec
sotirona <nedelinasot@gmail.com>
2017-04-14 21:59:04 +0300

    adding categories controller

---
+++ MyBlog/Controllers/Admin/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using MyBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Controllers.Admin
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            using(var database = new BlogDbContext())
+            {
+                var categories = database.Categories.ToList();
+
+                return View(categories);
+            }
+        }
+
+    }
+}
